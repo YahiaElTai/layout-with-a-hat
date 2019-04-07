a65437f7a3b87dbbca18e4d6be9329217d2452c8
Yahia El Tai <yahiaeltai114@gmail.com>
2019-04-07 21:57:58 +0100

    refactor helmetProps.js

--- src/helmetProps.js
+++ src/helmetProps.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-param-reassign */
 import { forEach, truncate, hasObjectItem } from './utilities';
 import addFavicon from './favicon';
 // Twitter title and description limitations
@@ -10,58 +9,39 @@ export default ({
   defaultTitle,
   description,
   image,
-  link,
-  meta,
-  og,
+  link = [],
+  meta = [],
+  og: ogParam = {},
   property,
   siteName,
   favicon,
   title,
   titleTemplate,
-  twitter,
+  twitter: twitterParam = {},
   url,
-  script,
-  style,
+  script = [],
+  style = [],
   ...rest
 }) => {
-  link = link || [];
-  meta = meta || [];
-  og = og || {};
-  twitter = twitter || {};
-  script = script || [];
-  style = style || [];
+  // og
+  const og = {
+    ...(siteName && { site_name: siteName }),
+    ...(title && { title }),
+    ...(description && { description }),
+    ...(image && { image }),
+    ...(url && { url }),
+    ...ogParam,
+  };
 
-  // siteName
-  if (siteName != null) {
-    if (defaultTitle == null) defaultTitle = siteName;
-    if (og.site_name == null) og.site_name = siteName;
-    if (!hasObjectItem(meta, 'name', 'application-name')) {
-      meta.push({ name: 'application-name', content: siteName });
-    }
-  }
+  // twitter
+  const twitter = {
+    ...(title && { title }),
+    ...(description && { description }),
+    ...(image && { image }),
+    ...(url && { url }),
+    ...twitterParam,
+  };
 
-  // favicon
-  if (favicon) {
-    addFavicon(favicon, meta, link);
-  }
-
-  // title
-  if (title != null) {
-    if (og.title == null) og.title = title;
-    if (twitter.title == null) twitter.title = title;
-  }
-  if (twitter.title) {
-    twitter.title = truncate(twitter.title, twitterTitleMaxLen, '...');
-  }
-
-  // description
-  if (description != null) {
-    if (og.description == null) og.description = description;
-    if (twitter.description == null) twitter.description = description;
-    if (!hasObjectItem(meta, 'name', 'description')) {
-      meta.push({ name: 'description', content: description });
-    }
-  }
   if (twitter.description) {
     twitter.description = truncate(
       twitter.description,
@@ -70,48 +50,63 @@ export default ({
     );
   }
 
+  if (twitter.title) {
+    twitter.title = truncate(twitter.title, twitterTitleMaxLen, '...');
+  }
+
+  // default title
+  // eslint-disable-next-line no-param-reassign
+  defaultTitle = defaultTitle || siteName;
+
+  // favicon
+  if (favicon) {
+    addFavicon(favicon, meta, link);
+  }
+
+  if (siteName && !hasObjectItem(meta, 'name', 'application-name')) {
+    meta.push({ name: 'application-name', content: siteName });
+  }
+  // description
+
+  if (description && !hasObjectItem(meta, 'name', 'description')) {
+    meta.push({ name: 'description', content: description });
+  }
+
   // image
-  if (image != null) {
-    if (og.image == null) og.image = image;
-    if (twitter.image == null) twitter.image = image;
-    if (!hasObjectItem(link, 'rel', 'apple-touch-startup-image')) {
-      link.push({ rel: 'apple-touch-startup-image', href: image });
-    }
+
+  if (image && !hasObjectItem(link, 'rel', 'apple-touch-startup-image')) {
+    link.push({ rel: 'apple-touch-startup-image', href: image });
   }
 
   // url
-  if (url != null) {
-    if (og.url == null) og.url = url;
-    if (twitter.url == null) twitter.url = url;
-    if (!hasObjectItem(link, 'rel', 'canonical')) {
-      link.push({ rel: 'canonical', href: url });
-    }
+
+  if (url && !hasObjectItem(link, 'rel', 'canonical')) {
+    link.push({ rel: 'canonical', href: url });
   }
 
-  // og
+  // set og
   forEach(
     og,
     (content, prop) =>
       content != null && meta.push({ property: `og:${prop}`, content }),
   );
 
-  // twitter
+  // set twitter
   forEach(
     twitter,
     (content, prop) =>
       content != null && meta.push({ property: `twitter:${prop}`, content }),
   );
 
-  // rest
-  if (base != null) {
-    rest.base = typeof base === 'string' ? { href: base } : base;
-  }
-  if (defaultTitle != null) rest.defaultTitle = defaultTitle;
-  if (link.length) rest.link = link;
-  if (meta.length) rest.meta = meta;
-  if (title != null) rest.title = title;
-  if (script.length) rest.script = script;
-  if (style.length) rest.style = style;
-
-  return rest;
+  return {
+    ...rest,
+    ...(defaultTitle && { defaultTitle }),
+    ...(link.length && { link }),
+    ...(meta.length && { meta }),
+    ...(title && { title }),
+    ...(script.length && { script }),
+    ...(style.length && { style }),
+    ...(base &&
+      (typeof base === 'string' ? { base: { href: base } } : { base })),
+  };
 };
